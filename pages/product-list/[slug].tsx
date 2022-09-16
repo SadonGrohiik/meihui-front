0adef6e8619ac2fa14dd152e404383dd817cb826
sadon <stiupfay@gmail.com>
2022-09-16 09:08:12 -0700

    s

--- pages/product-list/[slug].tsx
+++ pages/product-list/[slug].tsx
@@ -3,10 +3,22 @@ import { ProductCard } from "../../components/productCard";
 import { Context, gql } from "@apollo/client";
 import client from "../../apollo-client";
 import { Q_getPosts } from "../../util/queries";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-const ProductList: NextPage = ({ products }: any) => {
+const ProductList: NextPage = ({ products }: any, { slug }: any) => {
+  const router = useRouter();
+  console.log(router.asPath);
   return (
     <ul>
+      <Link
+        href={
+          router.asPath.substring(0, router.asPath.indexOf("?")) +
+          "?field=Price&order=asc"
+        }
+      >
+        Filter
+      </Link>
       {products &&
         products.map(
           (
@@ -29,19 +41,18 @@ const ProductList: NextPage = ({ products }: any) => {
 
 export async function getServerSideProps(context: Context) {
   const query = await context.query;
-  const sort = { field: await query.field, order: await query.order };
-
   let res = await getProductList(query.slug);
-  console.log(await query.pussy);
-  if (sort) {
+
+  if (query.field && query.order) {
+    const sort = { field: await query.field, order: await query.order };
     res = await getProductList(query.slug, [], sort);
   }
 
   const data = res.data;
-
   return {
     props: {
       products: data.categories.data[0].attributes.products.data,
+      slug: query.slug,
     },
   };
 }
